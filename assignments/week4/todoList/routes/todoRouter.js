28c37a7e3d30f995c5e42c075df01531e653cb36
https://github.com/bu-AprilW/fsw-110.git <april.william@bryanuniversity.edu>
2021-05-09 23:41:09 -0500

    todoList

--- assignments/week4/todoList/routes/todoRouter.js
+++ assignments/week4/todoList/routes/todoRouter.js
@@ -7,21 +7,21 @@ const todoItems = [
     {
         name: "Do the laundry",
         description: "Wash and dry clothes",
-        imageUrl: "",
+        imageUrl: "https://images.unsplash.com/photo-1517677208171-0bc6725a3e60?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8d2FzaGluZyUyMGxhdW5kcnklMjBhdCUyMGhvbWV8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
         completed: false,
         _id: uuidv4()
     },
     {
         name: "Start Dinner",
         description: "Put the pot roast in the crockpot",
-        imageUrl: "",
+        imageUrl: "https://images.unsplash.com/reserve/EnF7DhHROS8OMEp2pCkx_Dufer%20food%20overhead%20hig%20res.jpg?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fGNvb2tpbmd8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
         completed: true,
         _id: uuidv4()
     },
     {
         name: "Get dog food",
         description: "Buy some dog food",
-        imageUrl: "",
+        imageUrl: "https://images.unsplash.com/photo-1596491123074-fd69f1b7c12d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZG9nJTIwZm9vZHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
         completes: true,
         _id: uuidv4()
     },
